f80a6108da5bcbe68808499b2be3442170bb3510
Sunchi <rachanlamoolphal@gmail.com>
2021-09-01 17:53:32 +0700

    finish component trainning

--- components/Service/Trainning.vue
+++ components/Service/Trainning.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="textColor">
         <v-container>
             <v-row>
                 <v-col cols="12" md="6">
@@ -7,18 +7,36 @@
                         <v-img :src="train" contain></v-img>
                     </div>
                 </v-col>
-                <v-col cols="12" md="6">
+                <v-col cols="12" md="6" class="d-flex align-center">
                     <div>
-                        <div>Trainning</div>
-                        <div>Our IT training helps you solve real-world practical computing issues and practice for certification exams</div>
+                        <div class="title text-uppercase textAlign">-- Trainning --</div>
+                        <div class="display-2 textAlign my-4">Our IT training helps you solve real-world practical computing issues and practice for certification exams</div>
                         <div>
                             <v-row>
                                 <v-col cols="12" sm="6">
-                                    <div></div>
+                                    <div class="textAlign">
+                                        <v-icon color="#00999e">mdi-check</v-icon>
+                                        <span>Computer Network</span>
+                                    </div>
                                 </v-col>
-                                <v-col cols="12" sm="6"></v-col>
-                                <v-col cols="12" sm="6"></v-col>
-                                <v-col cols="12" sm="6"></v-col>
+                                <v-col cols="12" sm="6">
+                                    <div class="textAlign">
+                                        <v-icon color="#00999e">mdi-check</v-icon>
+                                        <span>Application Development</span>
+                                    </div>
+                                </v-col>
+                                <v-col cols="12" sm="6">
+                                    <div class="textAlign">
+                                        <v-icon color="#00999e">mdi-check</v-icon>
+                                        <span>Web development</span>
+                                    </div>
+                                </v-col>
+                                <v-col cols="12" sm="6">
+                                    <div class="textAlign">
+                                        <v-icon color="#00999e">mdi-check</v-icon>
+                                        <span>Web development</span>
+                                    </div>
+                                </v-col>
                             </v-row>
                         </div>
                     </div>
@@ -36,3 +54,23 @@ export default {
     }
 }
 </script>
+<style scoped>
+.OutLine{
+    border: 1px solid;
+}
+.fColor {
+  background-color: #00999e;
+  color: #fff;
+}
+.textColor{
+    color: #00999e;
+}
+.textAlign{
+    text-align: start;
+}
+@media only screen and (max-width:800px){
+    .textAlign{
+        text-align: center;
+    }
+}
+</style>
