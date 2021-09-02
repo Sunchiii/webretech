d2283a28cca60e9f1cb6ffad9768acecd8f26170
Sunchi <rachanlamoolphal@gmail.com>
2021-09-02 23:05:34 +0700

    finish form message

---
+++ components/contract/Inbox.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="my-16 fColor">
+    <v-container>
+      <div>
+        <div class="display-3 font-weight-bold text-center">Drop Us And Line</div>
+        <div class="subtitle-1 text-center my-5">
+          Reach out to us from our contact form and we will get back to you
+          shortly.
+        </div>
+        <div>
+          <v-form @submit.prevent="submit" ref="form" lazy-validation>
+            <v-row>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  v-model="name"
+                  :rules="nameRules"
+                  label="Name"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  v-model="lastname"
+                  :rules="nameRules"
+                  label="Lastname"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  v-model="email"
+                  :rules="emailRules"
+                  label="E-mail"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  v-model="phone"
+                  label="Phone"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-textarea v-model="discrip" autocomplete="Discription" label="Discription" outlined></v-textarea>
+              </v-col>
+              <v-col cols="12">
+                  <div class="text-center">
+                      <v-btn type="submit" @click="form" class="rounded-xl white--text" color="#00999e">Submit to send</v-btn>
+                  </div>
+              </v-col>
+            </v-row>
+          </v-form>
+        </div>
+      </div>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      nameRules: [(v) => !!v || 'Name is required'],
+      name: '',
+      lastname: '',
+      email: '',
+      phone: '',
+      discrip:'',
+      emailRules: [
+        (v) => !!v || 'E-mail is required',
+        (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+    }
+  },
+  methods:{
+      form(){
+      }
+  }
+}
+</script>
+<style scoped>
+.fColor{
+    color: #00999e;
+}
+</style>
--- pages/contract.vue
+++ pages/contract.vue
@@ -2,16 +2,19 @@
     <div>
         <Herotitle />
         <Map />
+        <Inbox />
     </div>
 </template>
 <script>
 import Herotitle from '../components/contract/Herotitle.vue'
 import Map from '../components/contract/Cardinfo.vue'
+import Inbox from '../components/contract/Inbox.vue'
 export default {
     layout:'topandfoot',
     components:{
         Herotitle,
-        Map
+        Map,
+        Inbox
     }
 }
 </script>
