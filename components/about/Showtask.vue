a9bb6f2b4de8ba6b7fc297d30d91c756a019e98a
Sunchi <rachanlamoolphal@gmail.com>
2021-09-06 19:40:05 +0700

    add our task

---
+++ components/about/Showtask.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="teal--text my-10">
+    <v-container>
+      <div>
+        <v-row>
+          <v-col cols="12" md="3" class="d-flex flex-column justify-center">
+            <div class="subtitle-2 font-weight-bold text-center">
+              COMPANY FACTS
+            </div>
+            <div class="display-1 text-center">We are proud</div>
+            <div class="display-1 text-center">of our works</div>
+          </v-col>
+          <v-col cols="12" md="3" class="d-flex flex-column justify-center align-center">
+            <div><v-icon size="100" color="teal">mdi-checkbox-multiple-marked </v-icon></div>
+            <div class="display-3">272+</div>
+            <div class="subtitle-1">complete projects</div>
+          </v-col>
+          <v-col cols="12" md="3" class="d-flex flex-column justify-center align-center">
+            <div><v-icon size="100" color="teal">mdi-account-multiple-check </v-icon></div>
+            <div class="display-3">72+</div>
+            <div class="subtitle-1">complete training</div>
+          </v-col>
+          <v-col cols="12" md="3" class="d-flex flex-column justify-center align-center">
+            <div><v-icon size="100" color="teal">mdi-clock </v-icon></div>
+            <div class="display-3">5+</div>
+            <div class="subtitle-1">Year in business</div>
+          </v-col>
+        </v-row>
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<style scoped>
+.fColor {
+  background-color: #00999e;
+}
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -3,18 +3,21 @@
     <Hero />
     <Discript />
     <Team />
+    <ST />
   </div>
 </template>
 <script>
 import Hero from '../components/about/Herotitle.vue'
 import Discript from '../components/about/Discription.vue'
 import Team from '../components/about/Team.vue'
+import ST from '../components/about/Showtask.vue'
 export default {
   layout: 'topandfoot',
   components: {
     Hero,
     Discript,
-    Team
+    Team,
+    ST
   },
 }
 </script>
