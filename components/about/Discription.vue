20e4c1c15f513b9fdbaaa5096da05e82bc5520b6
Sunchi <rachanlamoolphal@gmail.com>
2021-09-06 17:33:07 +0700

    add Disscription

---
+++ components/about/Discription.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="teal--text">
+        <v-container>
+            <v-divider></v-divider>
+            <div>
+                <div class="caption text-center my-5">A company turning ideas into beautiful things.</div>
+                <div class="text-center display-1 mx-auto" style="width:80%;">The business model for Retech was very simple but significant, to utilize Laos talents for Laos projects, an outsourcing model that has been used by countless agencies.</div>
+                <v-row class="my-10">
+                    <v-col cols="12" md="6">
+                        <v-card class="rounded-md pa-10 teal--text" elevation="5">
+                            <div class="text-center"><v-icon size="150" color="teal">mdi-remote-desktop</v-icon></div>
+                            <div class="text-center title-1">OFFICES</div>
+                            <div class="text-center title-1">IT SERVICE</div>
+                            <div class="text-center headline mx-auto font-weight-bold" style="width:60%;">We offer any IT online services that enable you to raise the qualify of your company.</div>
+                        </v-card>
+                    </v-col>
+                    <v-col cols="12" md="6">
+                        <v-card class="rounded-md pa-10 teal--text" elevation="5">
+                            <div class="text-center"><v-icon size="150" color="teal">mdi-bag-checked</v-icon></div>
+                            <div class="text-center title-1">IT PROBLEM</div>
+                            <div class="text-center title-1">IT SOLVING</div>
+                            <div class="text-center headline mx-auto font-weight-bold" style="width:60%;">We offer any IT online services that enable you to raise the qualify of your company.</div>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </div>
+            <v-divider></v-divider>
+        </v-container>
+    </div>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <Hero />
+    <Discript />
   </div>
 </template>
 <script>
 import Hero from '../components/about/Herotitle.vue'
+import Discript from '../components/about/Discription.vue'
 export default {
   layout: 'topandfoot',
   components: {
-    Hero
+    Hero,
+    Discript
   },
 }
 </script>
